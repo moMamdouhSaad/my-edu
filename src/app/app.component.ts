91d4a11b159b04c20d0332984a6c1997399ed30f
moMamdouhSaad <mo.mamdouh.saad@gmail.com>
2024-12-14 04:40:00 +0200

    add more styles files and adjust hero component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 import { IonApp, IonRouterOutlet, IonContent } from '@ionic/angular/standalone';
-import { HeaderComponent } from './shared/components/header/header.component';
-import { FooterComponent } from './shared/components/footer/footer.component';
+import { HeaderComponent } from './core/components/header/header.component';
+import { FooterComponent } from './core/components/footer/footer.component';
 
 @Component({
   selector: 'app-root',
